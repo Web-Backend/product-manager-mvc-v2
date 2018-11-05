50da98bef01aac4f7c1b54222d33c904e26a298d
long <longhoang94>
2018-11-05 21:28:03 +0700

    controller is me

---
+++ src/main/java/com/codegym/controllers/ProductController.java
@@ -0,0 +1,65 @@
+package com.codegym.controllers;
+
+import com.codegym.models.Product;
+import com.codegym.services.ProductService;
+import com.codegym.services.ProductServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProductController {
+    private ProductService productService = new ProductServiceImpl();
+
+    @GetMapping("/product")
+    public ModelAndView home() {
+        return new ModelAndView("index", "products", this.productService.findAll());
+    }
+
+    @GetMapping("product/create")
+    public ModelAndView create() {
+        return new ModelAndView("create", "product", new Product());
+    }
+
+    @PostMapping("product/save")
+    public ModelAndView save(Product product) {
+        product.setId((int) (Math.random()*99999));
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("index", "products", this.productService.findAll());
+        modelAndView.addObject("success", "Created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("product/{id}/edit")
+    public ModelAndView edit(@PathVariable int id) {
+        return new ModelAndView("edit", "product", this.productService.findById(id));
+    }
+
+    @PostMapping("product/update")
+    public ModelAndView update(Product product) {
+        productService.update(product.getId(), product);
+        ModelAndView modelAndView = new ModelAndView("index", "products", this.productService.findAll());
+        modelAndView.addObject("success", "Updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("product/{id}/delete")
+    public ModelAndView delete(@PathVariable int id) {
+        return new ModelAndView("delete", "product", productService.findById(id));
+    }
+
+    @PostMapping("/product/delete")
+    public ModelAndView delete(Product product) {
+        productService.remove(product.getId());
+        ModelAndView modelAndView = new ModelAndView("index", "products", productService.findAll());
+        modelAndView.addObject("success", "Deleted successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/product/{id}/view")
+    public ModelAndView view(Product product) {
+        return new ModelAndView("view", "product", productService.findById(product.getId()));
+    }
+}
